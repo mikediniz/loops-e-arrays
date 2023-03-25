0a09fcab24552dd5c6a77ecd27851f8be782a9f1
mikediniz <mikediniz.system@gmail.com>
2023-03-25 02:12:46 -0300

    Criação de loops

---
+++ src/br/com/dio/loops/Ex4_ParEImpar.java
@@ -0,0 +1,35 @@
+package br.com.dio.loops;
+
+import java.util.Scanner;
+
+/*
+Faça um programa que peça N números inteiros,
+calcule e mostre a quantidade de números pares
+e a quantidade de números impares.
+*/
+public class Ex4_ParEImpar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int quantNumeros;
+        int numero;
+        int numImpar = 0, numPares = 0;
+        int count = 0;
+
+        System.out.println("Quantidades de números: ");
+        quantNumeros = scan.nextInt();
+
+        do {
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+
+            if (numero % 2 == 0) numPares++;
+            else numImpar++;
+
+            count++;
+        }while (count < quantNumeros);
+
+        System.out.println("Quantidades de par: " + numPares);
+        System.out.println("Quantidades de impar: " + numImpar);
+    }
+}
